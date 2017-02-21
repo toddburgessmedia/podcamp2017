3ab843e4cd634df878d370acd34d179662b7d516
Todd Burgess <todd@toddburgessmedia.com>
2017-02-21 16:04:37 -0500

    First commit

---
+++ app/src/main/java/com/toddburgessmedia/podcamphelloworld/MainActivity.java
@@ -0,0 +1,17 @@
+package com.toddburgessmedia.podcamphelloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView tv = (TextView) findViewById(R.id.greeting);
+        tv.setText("Hello Podcamp");
+    }
+}
